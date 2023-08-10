47e4c5a76c86fb84a0164f92bdcc6563df16d2bb
Brian <wamwangimathenge@gmail.com>
2023-08-10 19:55:12 +0300

    Code a sub-class inheriting from the Person class
    This commit creates a sub-class, inheriting from the Person class.
    The worker class has 2 additional parameters in the constructor and
    all the parameters and methods of its super-class

--- ooprogramming.js
+++ ooprogramming.js
@@ -15,7 +15,17 @@ class Person {
     }
 }
 // Task 2: Code a Worker class
-
+class Worker extends Person{
+    constructor(xp=0, hourlyWage=10, name, age, energy) {
+        super(name, age, energy)
+        this.xp = xp
+        this.hourlyWage = hourlyWage
+    }
+    goToWork() {
+        this.xp += 10
+        console.log(this.xp)
+    }
+}
 // Task 3: Code an intern object, run methods
 
 
